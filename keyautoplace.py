433b00e6c0b6740374ee6791f9c1420aa4160186
phloamy <65916936+phloamy@users.noreply.github.com>
2021-07-18 18:12:26 -0700

    Made more features accessible through the dialog
    - different diode position and rotation
    - north facing switches
    - adjustable origin point
    - simple custom reference file (each reference separated by a new line)
    - "disable rotations" checkbox instead of "enable rotations" (now a stable feature)

--- keyautoplace.py
+++ keyautoplace.py
@@ -5,17 +5,20 @@ import sys
 import json
 import itertools
 import logging
+import math
 
 
 class KeyPlacer():
-    def __init__(self, logger, board, layout):
+    def __init__(self, logger, board, layout, referenceLabel, originPoint=wxPointMM(0, 0)):
         self.logger = logger
         self.board = board
         self.layout = layout
         self.keyDistance = 19050000
         self.currentKey = 1
         self.currentDiode = 1
-        self.referenceCoordinate = wxPoint(FromMM(25), FromMM(25))
+        self.referenceCoordinate = originPoint
+
+        self.referenceLabel = referenceLabel
 
 
     def GetModule(self, reference):
@@ -34,6 +37,18 @@ class KeyPlacer():
     
     def GetCurrentDiode(self, diodeFormat):
         diode = self.GetModule(diodeFormat.format(self.currentDiode))
+        self.currentDiode += 1
+        return diode
+
+
+    def GetCurrentKeyCustom(self, keyFormat):
+        key = self.GetModule(keyFormat.format(self.referenceLabel[self.currentKey - 1]))
+        self.currentKey += 1
+        return key
+
+
+    def GetCurrentDiodeCustom(self, diodeFormat):
+        diode = self.GetModule(diodeFormat.format(self.referenceLabel[self.currentDiode - 1]))
         self.currentDiode += 1
         return diode
 
@@ -73,25 +88,52 @@ class KeyPlacer():
         self.AddTrackSegment(segmentStart, [0, 10], layer=F_Cu)
 
 
-    def Run(self, keyFormat, diodeFormat, routeTracks=False, rotateModules=False):
+    def Run(self, keyFormat, diodeFormat, routeTracks=False, rotateModules=True, useNorthFacingSwitches=False, relativeDiodePosition=wx.RealPoint(0, 0), relativeDiodeRotation=270, useCustomAnnotationFormat=False):
         for key in self.layout["keys"]:
-            keyModule = self.GetCurrentKey(keyFormat)
-            position = wxPoint(self.referenceCoordinate.x + (self.keyDistance * key["x"]) + (self.keyDistance * key["width"] // 2), 
-                    self.referenceCoordinate.y + (self.keyDistance * key["y"]) + (self.keyDistance * key["height"] // 2))
+
+            position = wxPoint(
+                self.referenceCoordinate.x + (self.keyDistance * key["x"]) + (self.keyDistance * key["width"] // 2),
+                self.referenceCoordinate.y + (self.keyDistance * key["y"]) + (self.keyDistance * key["height"] // 2))
+
+            keyModule = None
+            if useCustomAnnotationFormat == True:
+                keyModule = self.GetCurrentKeyCustom(keyFormat)
+            else:
+                keyModule = self.GetCurrentKey(keyFormat)
+
             self.SetPosition(keyModule, position)
 
-            # something is not quite right, not recomented to use it yet:
-            if rotateModules == True:
-                rotationReference = wxPoint(self.referenceCoordinate.x + (self.keyDistance * key["rotation_x"]) + (self.keyDistance * key["width"] // 2), 
-                        self.referenceCoordinate.y + (self.keyDistance * key["rotation_y"]) + (self.keyDistance * key["height"] // 2))
-                keyModule.Rotate(rotationReference, key["rotation_angle"] * -10)
 
-            diodeModule = self.GetCurrentDiode(diodeFormat)
-            self.SetRelativePositionMM(diodeModule, position, [5.08, 3.03])
-            diodeModule.SetOrientationDegrees(270)
+            diodeModule = None
+            if useCustomAnnotationFormat == True:
+                diodeModule = self.GetCurrentDiodeCustom(diodeFormat)
+            else:
+                diodeModule = self.GetCurrentDiode(diodeFormat)
+
+            self.SetRelativePositionMM(diodeModule, position, [relativeDiodePosition.x, relativeDiodePosition.y])
 
             if not diodeModule.IsFlipped():
                 diodeModule.Flip(diodeModule.GetPosition())
+
+            if rotateModules == True:
+
+                rotationReference = wxPoint(
+                            self.referenceCoordinate.x + (self.keyDistance * key["rotation_x"]),
+                            self.referenceCoordinate.y + (self.keyDistance * key["rotation_y"])
+                            )
+
+                angle = key["rotation_angle"] * -10
+
+                if useNorthFacingSwitches == True:
+                    keyModule.SetOrientationDegrees(0)
+                else:
+                    keyModule.SetOrientationDegrees(180)
+                keyModule.Rotate(rotationReference, angle)
+                self.logger.info('Rotated key module to {}'.format(angle))
+
+                diodeModule.SetOrientationDegrees(relativeDiodeRotation)
+                diodeModule.Rotate(rotationReference, angle)
+                self.logger.info('Rotated diode module to {}'.format(angle))
 
             if routeTracks == True:
                 self.RouteKeyWithDiode(keyModule, diodeModule)
@@ -102,48 +144,94 @@ class KeyAutoPlaceDialog(wx.Dialog):
     def __init__(self, parent, title, caption):
         style = wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER
         super(KeyAutoPlaceDialog, self).__init__(parent, -1, title, style=style)
-        row1 = wx.BoxSizer(wx.HORIZONTAL)
 
-        text = wx.StaticText(self, -1, "Select kle json file:")
-        row1.Add(text, 0, wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, 5)
+        rows = []
+
+        for i in range(9):
+            rows.append(wx.BoxSizer(wx.HORIZONTAL))
+
+        # row 0
+        filePickerLabel = wx.StaticText(self, -1, "Select kle json file:")
+        rows[0].Add(filePickerLabel, 0, wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, 5)
 
         filePicker = wx.FilePickerCtrl(self, -1)
-        row1.Add(filePicker, 1, wx.EXPAND|wx.ALL, 5)
+        rows[0].Add(filePicker, 1, wx.EXPAND|wx.ALL, 5)
 
-        row2 = wx.BoxSizer(wx.HORIZONTAL)
-
+        # row 1
         keyAnnotationLabel = wx.StaticText(self, -1, "Key annotation format string:")
-        row2.Add(keyAnnotationLabel, 1, wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, 5)
+        rows[1].Add(keyAnnotationLabel, 1, wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, 5)
 
-        keyAnnotationFormat = wx.TextCtrl(self, value='MX_{}')
-        row2.Add(keyAnnotationFormat, 1, wx.EXPAND|wx.ALL, 5)
+        keyAnnotationFormat = wx.TextCtrl(self, value='K_{}')
+        rows[1].Add(keyAnnotationFormat, 1, wx.EXPAND | wx.ALL, 5)
 
-        row3 = wx.BoxSizer(wx.HORIZONTAL)
-
+        # row 2
         diodeAnnotationLabel = wx.StaticText(self, -1, "Diode annotation format string:")
-        row3.Add(diodeAnnotationLabel, 1, wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, 5)
+        rows[2].Add(diodeAnnotationLabel, 1, wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, 5)
 
         diodeAnnotationFormat = wx.TextCtrl(self, value='D_{}')
-        row3.Add(diodeAnnotationFormat, 1, wx.EXPAND|wx.ALL, 5)
+        rows[2].Add(diodeAnnotationFormat, 1, wx.EXPAND|wx.ALL, 5)
 
-        row4 = wx.BoxSizer(wx.HORIZONTAL)
-
+        # row 3
         tracksCheckbox = wx.CheckBox(self, label="Add tracks")
-        tracksCheckbox.SetValue(True)
-        row4.Add(tracksCheckbox, 1, wx.EXPAND|wx.ALL, 5)
+        tracksCheckbox.SetValue(False)
+        rows[3].Add(tracksCheckbox, 1, wx.EXPAND|wx.ALL, 5)
 
-        rotationCheckbox = wx.CheckBox(self, label="Enable rotations")
-        rotationCheckbox.SetValue(False)
-        row4.Add(rotationCheckbox, 1, wx.EXPAND|wx.ALL, 5)
+        disableRotationCheckbox = wx.CheckBox(self, label="Disable rotations")
+        disableRotationCheckbox.SetValue(False)
+        rows[3].Add(disableRotationCheckbox, 1, wx.EXPAND|wx.ALL, 5)
+
+        northFacingSwitchCheckbox = wx.CheckBox(self, label="Use north-facing switches")
+        rows[3].Add(northFacingSwitchCheckbox, 1, wx.EXPAND | wx.ALL, 5)
+
+        # row 4
+        diodePositionLabel = wx.StaticText(self, -1, "Relative diode position:")
+        rows[4].Add(diodePositionLabel, 1, wx.LEFT|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL, 5)
+
+        diodePositionX = wx.SpinCtrlDouble(self, value='0')
+        rows[4].Add(diodePositionX, 1, wx.EXPAND | wx.ALL, 5)
+
+        diodePositionY = wx.SpinCtrlDouble(self, value='-5.05')
+        rows[4].Add(diodePositionY, 1, wx.EXPAND | wx.ALL, 5)
+
+        # row 5
+        diodeRotationLabel = wx.StaticText(self, -1, "Relative diode rotation:")
+        rows[5].Add(diodeRotationLabel, 1, wx.LEFT | wx.RIGHT | wx.ALIGN_CENTER_VERTICAL, 5)
+
+        diodeRotation = wx.SpinCtrlDouble(self, value='180')
+        rows[5].Add(diodeRotation, 1, wx.EXPAND | wx.ALL, 5)
+
+        # row 6
+        originPointLabel = wx.StaticText(self, -1, "Origin point:")
+        rows[6].Add(originPointLabel, 1, wx.LEFT | wx.RIGHT | wx.ALIGN_CENTER_VERTICAL, 5)
+
+        originPointX = wx.SpinCtrlDouble(self, value='0')
+        rows[6].Add(originPointX, 1, wx.EXPAND | wx.ALL, 5)
+
+        originPointY = wx.SpinCtrlDouble(self, value='0')
+        rows[6].Add(originPointY, 1, wx.EXPAND | wx.ALL, 5)
+
+        # row 7
+        customAnnotationFormatCheckbox = wx.CheckBox(self, label="Use custom reference names")
+        customAnnotationFormatCheckbox.SetValue(True)
+        rows[7].Add(customAnnotationFormatCheckbox, 1, wx.EXPAND | wx.ALL, 5)
+
+        customAnnotationSimpleFileCheckbox = wx.CheckBox(self, label="Use simple reference name file") # list references in a txt file and separate each reference with a new line
+        customAnnotationSimpleFileCheckbox.SetValue(True)
+        rows[7].Add(customAnnotationSimpleFileCheckbox, 1, wx.EXPAND | wx.ALL, 5)
+
+        # row 8
+        customAnnotationLabel = wx.StaticText(self, -1, "Select custom annotation json file:")
+        rows[8].Add(customAnnotationLabel, 0, wx.LEFT | wx.RIGHT | wx.ALIGN_CENTER_VERTICAL, 5)
+
+        filePicker2 = wx.FilePickerCtrl(self, -1)
+        rows[8].Add(filePicker2, 1, wx.EXPAND | wx.ALL, 5)
 
         box = wx.BoxSizer(wx.VERTICAL)
 
-        box.Add(row1, 0, wx.EXPAND|wx.ALL, 5)
-        box.Add(row2, 0, wx.EXPAND|wx.ALL, 5)
-        box.Add(row3, 0, wx.EXPAND|wx.ALL, 5)
-        box.Add(row4, 0, wx.EXPAND|wx.ALL, 5)
+        for row in rows:
+            box.Add(row, 0, wx.EXPAND|wx.ALL, 5)
 
-        buttons = self.CreateButtonSizer(wx.OK|wx.CANCEL)
+        buttons = self.CreateButtonSizer(wx.OK | wx.CANCEL)
         box.Add(buttons, 0, wx.EXPAND|wx.ALL, 5)
 
         self.SetSizerAndFit(box)
@@ -151,7 +239,16 @@ class KeyAutoPlaceDialog(wx.Dialog):
         self.keyAnnotationFormat = keyAnnotationFormat
         self.diodeAnnotationFormat = diodeAnnotationFormat
         self.tracksCheckbox = tracksCheckbox
-        self.rotationCheckbox = rotationCheckbox
+        self.disableRotationCheckbox = disableRotationCheckbox
+        self.northFacingSwitchChekbox = northFacingSwitchCheckbox
+        self.diodePositionX = diodePositionX
+        self.diodePositionY = diodePositionY
+        self.diodeRotation = diodeRotation
+        self.originPointX = originPointX
+        self.originPointY = originPointY
+        self.customAnnotationFormatCheckbox = customAnnotationFormatCheckbox
+        self.customAnnotationSimpleFileCheckbox = customAnnotationSimpleFileCheckbox
+        self.filePicker2 = filePicker2
 
 
     def GetJsonPath(self):
@@ -171,13 +268,41 @@ class KeyAutoPlaceDialog(wx.Dialog):
 
 
     def IsRotation(self):
-        return self.rotationCheckbox.GetValue()
+        return not self.disableRotationCheckbox.GetValue()
+
+
+    def IsNorthFacing(self):
+        return self.northFacingSwitchChekbox.GetValue()
+
+
+    def GetRelativeDiodePosition(self):
+        return wx.RealPoint(self.diodePositionX.GetValue(), self.diodePositionY.GetValue())
+
+
+    def GetRelativeDiodeRotation(self):
+        return self.diodeRotation.GetValue()
+
+
+    def GetOriginPoint(self):
+        return wxPointMM(self.originPointX.GetValue(), self.originPointY.GetValue())
+
+
+    def IsUsingCustomAnnotationFormat(self):
+        return self.customAnnotationFormatCheckbox.GetValue()
+
+
+    def IsUsingSimpleFile(self):
+        return self.customAnnotationSimpleFileCheckbox.GetValue()
+
+
+    def GetCustomAnnotationPath(self):
+        return self.filePicker2.GetPath()
 
         
 class KeyAutoPlace(ActionPlugin):
     def defaults(self):
-        self.name = "KeyAutoPlace"
-        self.category = "Mechanical Keybaord Helper"
+        self.name = "KeyAutoPlaceRevamp"
+        self.category = "Mechanical Keyboard Helper"
         self.description = "Auto placement for key switches and diodes"
 
 
@@ -214,8 +339,31 @@ class KeyAutoPlace(ActionPlugin):
             f.close()
             layout = json.loads(textInput)
             self.logger.info("User layout: {}".format(layout))
-            placer = KeyPlacer(self.logger, self.board, layout)
-            placer.Run(dlg.GetKeyAnnotationFormat(), dlg.GetDiodeAnnotationFormat(), dlg.IsTracks(), dlg.IsRotation())
+
+            referenceLabel = None
+
+            if dlg.IsUsingCustomAnnotationFormat() == True:
+
+                annotationLabelPath = dlg.GetCustomAnnotationPath()
+
+                if dlg.IsUsingSimpleFile():
+                    referenceLabel = []
+
+                    with open(annotationLabelPath) as file:
+                        for line in file:
+                            referenceLabel.append(line.replace("\n", ""))
+
+                    self.logger.info("User annotations: {}".format(referenceLabel))
+
+                else:
+                    f2 = open(annotationLabelPath, "r")
+                    textInput2 = f2.read()
+                    f2.close()
+                    referenceLabel = json.loads(textInput2)["annotations"]
+                    self.logger.info("User annotations: {}".format(referenceLabel))
+
+            placer = KeyPlacer(self.logger, self.board, layout, referenceLabel, dlg.GetOriginPoint())
+            placer.Run(dlg.GetKeyAnnotationFormat(), dlg.GetDiodeAnnotationFormat(), dlg.IsTracks(), dlg.IsRotation(), dlg.IsNorthFacing(), dlg.GetRelativeDiodePosition(), dlg.GetRelativeDiodeRotation(), dlg.IsUsingCustomAnnotationFormat())
 
 
         dlg.Destroy()
